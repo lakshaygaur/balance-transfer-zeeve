cb27df9809d5746f8a7d299380ba56664689bcb0
sanyam gupta <sanyam.gupta@sofocle.com>
2020-02-13 15:05:37 +0530

    removing endosment policy

--- app/instantiate-chaincode.js
+++ app/instantiate-chaincode.js
@@ -52,15 +52,6 @@ const instantiateChaincode = async function(peers, channelName, chaincodeName, c
 
 			// Use this to demonstrate the following policy:
 			// The policy can be fulfilled when members from both orgs signed.
-			'endorsement-policy': {
-			        identities: [
-					{ role: { name: 'member', mspId: 'Org1MSP' }},
-					{ role: { name: 'member', mspId: 'Org2MSP' }}
-			        ],
-			        policy: {
-					'2-of':[{ 'signed-by': 0 }, { 'signed-by': 1 }]
-			        }
-		        }
 		};
 
 		if (functionName)
